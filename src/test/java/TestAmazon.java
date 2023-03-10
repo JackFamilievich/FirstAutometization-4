6cba984c3bea2616445961eb078e57b749e84bd5
Jack Familievich <jackfamilievich@MacBook-Air-Jack.local>
2023-03-10 17:21:35 +0200

    firsttest

--- src/test/java/TestAmazon.java
+++ src/test/java/TestAmazon.java
@@ -2,14 +2,17 @@ package test.java;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class TestAmazon {
     WebDriver driver;
     String url = "https://www.amazon.com/";
+
     public void openHamburger(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         driver = new ChromeDriver();
@@ -27,7 +30,20 @@ public class TestAmazon {
         WebElement VideoGames = driver.findElement(By.linkText("Video Games"));
         js.executeScript("arguments[0].scrollIntoView();",VideoGames);
         driver.findElement(By.xpath("//a[@data-menu-id='26']")).click();
-        
+        WebElement SearchBar = driver.findElement(By.xpath("//input[@name='field-keywords']"));
+        SearchBar.sendKeys("Java", Keys.ENTER);
+        driver.findElement(By.xpath("//a[@class='a-link-normal s-no-outline']")).click();
+        driver.findElement(By.xpath("//a[@id='breadcrumb-back-link']")).click();
+        WebElement filterEnglish = driver.findElement(By.linkText("English"));
+        js.executeScript("arguments[0].scrollIntoView();",filterEnglish);
+        driver.findElement(By.xpath("//a[@href='/s?k=java&i=stripbooks&rh=n%3A283155%2Cp_n_feature_nine_browse-bin%3A3291437011&dc&crid=2ACQU6U6L6G8R&qid=1678457838&rnid=3291435011&sprefix=ja%2Caps%2C198&ref=sr_nr_p_n_feature_nine_browse-bin_1&ds=v1%3AA5TGIYzxH3OCFyHf%2Fye0D9CYqPEeXRxOZNcAFYSaUVc']")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        int pageAmount = driver.findElements(By.xpath("//span[@class='s-pagination-item s-pagination-disabled']")).size();
+        Assert.assertEquals(size,1);
+        driver.findElement(By.xpath("//a[@class='a-link-normal s-no-outline']")).click();
+        driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+
+
         driver.quit();
 
 
